367c9cbf4513c67c8995a7338586cc4d5d949cfe
edGlobant <eduardo.orihuela@globant.com>
2023-07-14 17:06:29 -0500

    Changes and impproves on swagger

--- src/main/java/com/springwebflux/config/SwaggerConfig.java
+++ src/main/java/com/springwebflux/config/SwaggerConfig.java
@@ -1,17 +1,24 @@
 package com.springwebflux.config;
 
+import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
+
     @Bean
-    public OpenAPI springConvertOpenAPI() {
+    public OpenAPI springWebfluxOpenAPI() {
         return new OpenAPI()
                 .info(new Info().title("Vehicle usd to crypto price converter API Docs")
                         .description("REST API documentation")
-                        .version("v1.0.0"));
+                        .version("v1.0.0")
+                        .license(new License().name("No license").url("")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("API Git Documentation")
+                        .url("https://github.com/edGlobant/springboot-webflux-postgresql-convert"));
     }
 }
